b2aba8058b76a12a9514d791b83301efb2c4dd03
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-15 10:59:20 -0500

    Update code.py

--- Lesson4/code.py
+++ Lesson4/code.py
@@ -2,7 +2,7 @@
 
 # Created by Ryan Chung Kam Chung
 # Created in January 2021
-# Setting background on the PyBadge
+# Constraining character on the screen
 
 
 # Libraries that will enable us to render and stage assets
