024ee91fa16ce31d35f2fe62ea56f032e6e1df2e
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-18 16:55:29 -0500

    Update code.py

--- Lesson5/code.py
+++ Lesson5/code.py
@@ -16,27 +16,17 @@ import constants
 def game_scene():
     # this function is the main game scene
 
-    # Image bank holds all image assets used
+    # -------------------------------- Assets ----------------------------------
+    # Image banks
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
-
-    # Buttons with state information
-    a_button = constants.button_state["button_up"]
-    b_button = constants.button_state["button_up"]
-    start_button = constants.button_state["button_up"]
-    select_button = constants.button_state["button_up"]
-
-    # get sound ready
-    pew_sound = open("pew.wav", 'rb')
-    sound = ugame.audio
-    sound.stop()
-    sound.mute(False)
-
+    
+    # ------------------------------ Background --------------------------------
     # Sets background to the 0th image in the image bank
     # 10x8 grid
     background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X,
                               constants.SCREEN_GRID_Y)
-
+    
     # Sets the floor as the 1st image in the image bank, the walls are still going
     # to be the 0th image
     for x_location in range(1, constants.SCREEN_GRID_X - 1):
@@ -44,18 +34,42 @@ def game_scene():
             tile_picked = 1
             background.tile(x_location, y_location, tile_picked)
 
+    # --------------------------------- Sound -----------------------------------
+    # Shooting sound
+    pew_sound = open("pew.wav", 'rb')
+    
+    # Sound setup
+    sound = ugame.audio
+    # Stop all sound
+    sound.stop()
+    # Unmute
+    sound.mute(False)
+    
+    # ------------------------------ Button States ------------------------------
+    
+    # Buttons with state information
+    a_button = constants.button_state["button_up"]
+    b_button = constants.button_state["button_up"]
+    start_button = constants.button_state["button_up"]
+    select_button = constants.button_state["button_up"]
+    
+    # ----------------------------- Sprites creation -----------------------------
     # Ship sprite being displayed
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-    # List of lasers
+    # List of bullets
     bullets = []
+    bullet_direction = []
     for bullets_number in range(constants.TOTAL_NUMBER_OF_BULLETS):
         a_single_bullet = stage.Sprite(image_bank_sprites, 10,
                                      constants.OFF_SCREEN_X,
                                      constants.OFF_SCREEN_Y)
         bullets.append(a_single_bullet)
+        # Sets bullet direction
+        bullet_direction.append("")
+        direction = "Up"
 
-
+    # ----------------------------- Stage and render ------------------------------
     # Creates a stage for the background
     # Sets frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
@@ -67,13 +81,13 @@ def game_scene():
     # Usually you should render background once per scene
     game.render_block()
 
+    # --------------------------------- Game Loop ----------------------------------
     while True:
-        # Game loop
 
-        # User input
+    # --------------------------------- User input ---------------------------------
         keys = ugame.buttons.get_pressed()
 
-        # A button to fire
+        # Button states to fire
         if keys & ugame.K_X != 0:
             if a_button == constants.button_state["button_up"]:
                 a_button = constants.button_state["button_just_pressed"]
@@ -90,45 +104,78 @@ def game_scene():
                 ship.move(ship.x + constants.SPRITE_MOVEMENT_SPEED, ship.y)
             else:
                 ship.move(constants.SCREEN_X - 2 * constants.SPRITE_SIZE, ship.y)
+            direction = "Right"
         if keys & ugame.K_LEFT:
             # Move left with constraints of the left border
             if ship.x >= constants.SCREEN_X - 9 * constants.SPRITE_SIZE:
                 ship.move(ship.x - constants.SPRITE_MOVEMENT_SPEED, ship.y)
             else:
                 ship.move(constants.SPRITE_SIZE, ship.y)
+            direction = "Left"
         if keys & ugame.K_UP:
             # Moves down with constraints of the ceiling
             if ship.y >= constants.SPRITE_SIZE:
                 ship.move(ship.x, ship.y - constants.SPRITE_MOVEMENT_SPEED)
             else:
                 ship.move(ship.x, constants.SPRITE_SIZE)
+            direction = "Up"
         if keys & ugame.K_DOWN:
             # Moves down with constraints of the ground
             if ship.y <= constants.SCREEN_Y - 2 * constants.SPRITE_SIZE:
                 ship.move(ship.x, ship.y + constants.SPRITE_MOVEMENT_SPEED)
             else:
                 ship.move(ship.x, constants.SCREEN_Y - 2 * constants.SPRITE_SIZE)
-
-        # Update Game Logic
+            direction = "Down"
 
         # Shoot with sound
         if a_button == constants.button_state["button_just_pressed"]:
             for bullet_number in range(len(bullets)):
                 if bullets[bullet_number].x < 0:
                     bullets[bullet_number].move(ship.x, ship.y)
-                    sound.play(pew_sound)
+                    bullet_direction[bullet_number] = direction
+                    # sound.play(pew_sound)
                     break
 
+    # ----------------------------- Bullet movement ------------------------------
         # When bullets get shot, check if they are off the screen.
         for bullet_number in range(len(bullets)):
-            if bullets[bullet_number].x > 0:
-                bullets[bullet_number].move(bullets[bullet_number].x,
-                                          bullets[bullet_number].y -
-                                          constants.BULLET_SPEED)
-                if bullets[bullet_number].y < constants.OFF_TOP_SCREEN:
-                    bullets[bullet_number].move(constants.OFF_SCREEN_X,
-                                              constants.OFF_SCREEN_Y)
+            if bullets[bullet_number].x > -1 * constants.SPRITE_SIZE:
+                if bullet_direction[bullet_number] == "Up": 
+                    bullets[bullet_number].move(bullets[bullet_number].x,
+                                                bullets[bullet_number].y
+                                                - constants.BULLET_SPEED)
+                if bullet_direction[bullet_number] == "Down":
+                    bullets[bullet_number].move(bullets[bullet_number].x,
+                                                bullets[bullet_number].y
+                                                + constants.BULLET_SPEED)
+                if bullet_direction[bullet_number] == "Left":
+                    bullets[bullet_number].move(bullets[bullet_number].x
+                                                - constants.BULLET_SPEED,
+                                                bullets[bullet_number].y)
+                if bullet_direction[bullet_number] == "Right":
+                    bullets[bullet_number].move(bullets[bullet_number].x
+                                                + constants.BULLET_SPEED,
+                                                bullets[bullet_number].y)
+            
+            # Move back bullets to "staging" if they are too far from the ship
+            # Right
+            if bullets[bullet_number].x > ship.x + constants.SPRITE_SIZE:
+                bullets[bullet_number].move(constants.OFF_SCREEN_X,
+                                            constants.OFF_SCREEN_Y)
+            # Right
+            if bullets[bullet_number].x < ship.x - constants.SPRITE_SIZE:
+                bullets[bullet_number].move(constants.OFF_SCREEN_X,
+                                            constants.OFF_SCREEN_Y)
 
+            if bullets[bullet_number].y > ship.y + constants.SPRITE_SIZE:
+                bullets[bullet_number].move(constants.OFF_SCREEN_X,
+                                            constants.OFF_SCREEN_Y)
+
+            if bullets[bullet_number].y < ship.y - constants.SPRITE_SIZE:
+                bullets[bullet_number].move(constants.OFF_SCREEN_X,
+                                            constants.OFF_SCREEN_Y)
+
+    # ----------------------------- Render and redraw ------------------------------
         # Renders and redraws the ship
         game.render_sprites(bullets + [ship])
         # Waits until refresh rate finishes
